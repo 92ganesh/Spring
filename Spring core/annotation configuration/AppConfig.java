527c14380e574c52d7560b7f62a77be153498a8b
Ganesh Patra <ganesh.patra@somaiya.edu>
2018-12-27 02:37:37 +0530

    appConfig

---
+++ Spring core/annotation configuration/AppConfig.java
@@ -0,0 +1,31 @@
+/**
+ * NOTE:- 
+ * name of the method can be anything
+ * name of this file name i.e AppConfig can be anything
+ * only the word Samsung is mapped 
+ */
+
+
+package com.ganesh.SpringTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	/** Bean is created like 
+	 * @Bean
+	 * public Interface/Class_name anyMethodName() {
+	 * 	  return new Class_name();
+	 * }
+	 */
+	
+	@Bean
+	public Samsung getPhone() {  
+		return new Samsung();
+	}
+	
+	@Bean
+	public MobileProcessor getProcessor() {
+		return new Snapdragon();
+	}
+} 
